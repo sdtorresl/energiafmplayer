895d8340cea2c643328b78c2dc2b0131601102df
Sergio Torres <sdtorresl@unal.edu.co>
2014-01-06 12:48:58 -0500

    Corregidas etiquetas HTML, agregadas seccion de podcast y programas, funcionamiento de jPlayer

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
 		<jdoc:include type="head" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
-
-		<!-- <title>Energia FM Player</title> -->
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/favicon.ico" type="image/x-icon">
 
 		<!-- Stylesheets -->
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/reset.css">
@@ -29,220 +31,257 @@
 	<body>
 		<!-- Facebook code -->
 		<div id="fb-root"></div>
-		<script>
-			(function(d, s, id) {
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) return;
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));
-		</script>
 
-		<script>
-		    (function($){
-		        $(window).load(function(){
-		            $("#newsContainer").mCustomScrollbar();
-		        });
-		    })(jQuery);
-		</script>
+		<div class="pageContainer">
 
-		<header>
-			<div id="header">
-				<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/logo.png" alt="logo" id="logo">
-				<div id="playerContainer">			
-					<div id="cover"></div>
+			<header>
+				<div id="header">
+					<a href="http://energia.fm/online">
+						<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/logo.png" alt="logo" id="logo">
+					</a>
 
-					<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+					<div id="playerContainer">			
+						<div id="cover">
+							<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/no-cover.png" alt="">
+						</div>
 
-					<div id="jp_container_1" class="jp-audio">
-					    <div class="jp-type-single">
-					        <div class="jp-gui jp-interface">
-					            <ul class="jp-controls">
+						<div id="jquery_jplayer_1" class="jp-jplayer"></div>
 
-					                <!-- comment out any of the following <li>s to remove these buttons -->
-
-					                <li><a href="javascript:;" class="jp-play" tabindex="1">
-					                		<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png" alt="play">
-					                	</a>
-					                </li>
-					                <li>
-					                	<a href="javascript:;" class="jp-mute" tabindex="1" title="mute">
-					                		<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-volume.png" alt="mute">
-					                	</a>
-					                </li>
-					            </ul>
-					            
-					            <!-- you can comment out any of the following <div>s too -->
-
-					            <div class="jp-volume-bar">
-					                <div class="jp-volume-bar-value"></div>
-					            </div>
-					            <div class="jp-current-time"></div>                
+						<div id="jp_container_1" class="jp-audio">
+							<div class="jp-artist">
+					            <p>Energía FM</p>
 					        </div>
-					        <div class="jp-title">
-					            <ul>
-					                <li>Energía FM</li>
-					            </ul>
-					        </div>
-					        <div class="jp-no-solution">
-					            <span>Actualización requerida</span>
-					            Para reproducir este contenido usted necesita adquirir la versión más reciente de su navegador o actualizar su versión del plugin de <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash Player</a>.
-					        </div>
-					    </div>
-					</div>
 
-					<div id="shareSong">
-						<p>Compartir canción en 
-							<span id="facebookShare"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-fb.png" alt="facebook"></span>
-							<span><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-tw.png" alt="tweeter"></span>
-						</p>
+							<div class="jp-title">
+					            <p>Energía FM</p>
+					        </div>
+
+							<div class="jp-controls">	
+								<div class="control">
+								    <a href="javascript:;" class="jp-play" tabindex="1">
+								    	<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png" alt="play">
+								    </a>
+								    <a href="javascript:;" class="jp-pause" tabindex="1">
+								    	<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-pause.png" alt="pause">
+								    </a>
+								</div>
+
+								<div class="control">
+								    <a href="javascript:;" class="jp-mute" tabindex="1" title="mute">
+								    	<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-volume.png" alt="mute">
+								    </a>
+								</div>
+
+								<div class="jp-volume-bar">
+									<div class="jp-volume-bar-value"></div>
+								</div>
+							</div>
+						    
+						    <div class="jp-nextSong">
+						    	<h3>Siguiente canción</h3>
+						    	<p>Artista- canción</p>
+						    </div>
+
+							<div class="jp-no-solution">
+								<span>Actualización requerida</span>
+								Para reproducir este contenido usted necesita adquirir la versión más reciente de su navegador o actualizar su versión del plugin de <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash Player</a>.
+							</div>
+						</div>
+
+						<div id="shareSong">
+							<p>Compartir canción en 
+								<span id="facebookShare">
+									<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fenergia.fm/online/reproductor">
+										<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-fb.png" alt="facebook">
+									</a>
+								</span>
+								<span id="twitterShare">
+									<a target="_blank" href="twitter.com">
+										<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-tw.png" alt="twitter">
+									</a>
+									
+								</span>
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
-		</header>
+			</header>
 
-		<div id="main">
-			<div id="program" class="upperBoxes box">
-				<h1 id="programTittle">Programación</h1>
-				<ul>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-					<li>
-						<h2>Nombre Programa</h2>
-						<p>Lun. a Vie. de 6am a 12pm</p>
-					</li>
-				</ul>
-			</div>
-
-			<div id="news" class="upperBoxes box">
-				<h1 id="newsTittle">Noticias</h1>
-				<div id="newsContainer">
+			<section id="main">
+				<article id="program" class="upperBoxes box">
+					<h1 id="programTittle">Programación</h1>
 					<ul>
-						<li class="itemNews">
-							<img class="imgNews" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/news.png">
-							<h2>Lorem ipsum dolor</h2>
-							<p>Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-							<a href="" class="readMore">Leer más</a>
-						</li>
-						<li class="itemNews">
-							<img class="imgNews" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/news.png">
-							<h2>Lorem ipsum dolor</h2>
-							<p>Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-							<a href="" class="readMore">Leer más</a>
-						</li>
-						<li class="itemNews">
-							<img class="imgNews" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/news.png">
-							<h2>Lorem ipsum dolor</h2>
-							<p>Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-							<a href="" class="readMore">Leer más</a>
-						</li>
-						<li class="itemNews">
-							<img class="imgNews" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/news.png">
-							<h2>Lorem ipsum dolor</h2>
-							<p>Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-							<a href="" class="readMore">Leer más</a>
-						</li>
-						<li class="itemNews">
-							<img class="imgNews" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/news.png">
-							<h2>Lorem ipsum dolor</h2>
-							<p>Ut enim ad minim veniam,
-							quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-							<a href="" class="readMore">Leer más</a>
-						</li>
+						<!-- List the programs -->
+						<?php
+							$catid = 3; // Programs category
+
+							// Get a db connection.
+							$db = JFactory::getDbo();
+							 
+							// Create a new query object.
+							$query = $db->getQuery(true);
+							 
+							// Select all records from the user profile table where key begins with "custom.".
+							// Order it by the ordering field.
+							$query->select($db->quoteName(array('id', 'title', 'introtext', 'published')));
+							$query->from($db->quoteName('#__k2_items'));
+							$query->where($db->quoteName('catid') . ' = '. $db->quote($catid));
+							$query->order('id DESC');
+						
+							// Reset the query using our newly populated query object.
+							$db->setQuery($query);
+							 
+							// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+							$results = $db->loadObjectList();
+							
+							$row = $db->loadRowList();
+							foreach ($row as $program => $item) {
+								// Only show published items
+								if ($item[3] == "1") {
+									echo "<li>";
+									echo "<h2>$item[1]</h2>";
+									echo "$item[2]";
+									echo "</li>";
+								}
+							}
+						?>
 					</ul>
+				</article>
+
+				<article id="news" class="upperBoxes box">
+					<h1 id="newsTittle">Noticias</h1>
+					<div id="newsContainer">
+						<jdoc:include type="component" />
+					</div>
+				</article>
+
+				<article id="podcasts" class="upperBoxes box">
+					<h1 id="podcastsTittle">Podcasts</h1>
+					
+					<div id="podcastContainer">
+						<ul class="listPodcasts">
+
+							<?php
+							$catid = 2; // Programs category
+
+							// Get a db connection.
+							$db = JFactory::getDbo();
+							 
+							// Create a new query object.
+							$query = $db->getQuery(true);
+							 
+							// Select all records from the user profile table where key begins with "custom.".
+							// Order it by the ordering field.
+							$query->select($db->quoteName(array('id', 'title', 'introtext', 'published', 'extra_fields_search')));
+							$query->from($db->quoteName('#__k2_items'));
+							$query->where($db->quoteName('catid') . ' = '. $db->quote($catid));
+							$query->order('id DESC');
+						
+							// Reset the query using our newly populated query object.
+							$db->setQuery($query);
+							 
+							// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+							$results = $db->loadObjectList();
+							
+							$row = $db->loadRowList();
+							foreach ($row as $key => $item) {
+								// Only show published items
+								if ($item[3] == "1") {
+
+									if (($key+1) % 2 == 0) 
+										$class = "even";
+									else
+										$class  = "odd";
+									
+									$playerClass = $key +2; 
+
+									// Get the link of the media
+									$link = $item[4];
+
+									?>
+
+									<!-- Put player scripts for all podcasts -->
+									<script type="text/javascript">
+									$(document).ready(function() {
+										$("<?php echo '#jquery_jplayer_' . $playerClass; ?>").jPlayer({
+											ready: function(event) {
+												$(this).jPlayer("setMedia", {
+													mp3: "<?php echo $link; ?>"
+												});
+											},
+											swfPath: "http://jplayer.org/latest/js",
+											supplied: "mp3",
+											cssSelectorAncestor: "<?php echo '#jp_container_' . $playerClass; ?>",
+										});
+									});
+									</script>
+
+									<div id="<?php echo 'jquery_jplayer_' . $playerClass; ?>" class="jp-jplayer"></div>
+									<div id="<?php echo 'jp_container_' . $playerClass; ?>" class="jp-audio">
+										<li class='<?php echo $class; ?>'>
+											<h2><?php echo $item[1]; ?></h2>
+
+											<?php echo $item[2]; ?>
+
+											<span class='podcastPlay'>
+												<a href="javascript:;" class="jp-play" tabindex="1">
+													<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png">
+												</a>
+												<a href="javascript:;" class="jp-pause" tabindex="1">
+													<img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-pause.png">
+												</a>
+											</span>
+										</li>
+									</div>
+								<?php
+								}
+							}
+							?>
+						</ul>
+					</div>
+				</article>
+
+
+				<aside id="facebook" class="lowerBoxes box">
+					<h1 id="facebookTittle">Síguenos en Facebook</h1>
+					<div class="fb-like-box" data-href="https://www.facebook.com/EnergiaFmOnline" data-width="270" data-height="220" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
+				</aside>
+
+				<aside id="tweets" class="lowerBoxes box">
+					<a class="twitter-timeline"  href="https://twitter.com/EnergiaFmOnline"  data-widget-id="418782439649972224">Tweets</a>	    			
+				</aside>
+
+				<article id="contact" class="lowerBoxes box">
+					<h1 id="contactTittle">Contacto</h1>
+					<ul class="contactText">
+						<li>Cra 123 Risque 67-89</li>
+						<li>storres@exeamedia.com</li>
+						<li>3251888</li>
+						<li>3152238974</li>
+					</ul>
+				</article>
+
+				<aside id="networks" class="lowerBoxes box">
+					<h1 id="networksTittle">Redes         
+						<div class="socialImg">
+							<a target="_blank" href="https://twitter.com/EnergiaFmOnline" id="twButton"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-tw.png" alt="">		
+							<a target="_blank" href="https://www.facebook.com/EnergiaFmOnline" id="fbButton"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-fb.png" alt="" width="7px"></a>
+						</div>     
+					</h1>
+				</aside>
+			</section>	
+
+			<footer>
+				<div id="info">
+					<div id="license">
+						<p>Copyright 2013. Energia. Todos los derechos reservados</p>
+					</div>
+					<div id="credits">
+						<p>Desarrollado por <a target="_blank" href="http://exeamedia.com">Exea</a></p>
+					</div>
 				</div>
-			</div>
-
-			<div id="podcasts" class="upperBoxes box">
-				<h1 id="podcastsTittle">Podcasts</h1>
-				<ul>
-					<li class="odd">
-						<h2>Lorem ipsum</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						<span class="podcastPlay"><a href="#"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png"></a></span>
-					</li>
-					<li class="even">
-						<h2>Lorem ipsum</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						<span class="podcastPlay"><a href="#"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png"></a></span>
-					</li>
-					<li class="odd">
-						<h2>Lorem ipsum</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						<span class="podcastPlay"><a href="#"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png"></a></span>
-					</li>
-					<li class="even">
-						<h2>Lorem ipsum</h2>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-						<span class="podcastPlay"><a href="#"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-play.png"></a></span>
-					</li>
-				</ul>
-			</div>
-
-			<div id="facebook" class="lowerBoxes box">
-				<h1 id="facebookTittle">Síguenos en Facebook</h1>
-				<div class="fb-like-box" data-href="https://www.facebook.com/EnergiaFmOnline" data-width="270" data-height="220" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
-			</div>
-
-			<div id="tweets" class="lowerBoxes box">
-				<!-- <h1 id="tweetsTittle">Tweets</h1> -->
-				<a class="twitter-timeline"  href="https://twitter.com/EnergiaFmOnline"  data-widget-id="418782439649972224">Tweets por @EnergiaFmOnline</a>
-    			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-    			</script>
-			</div>
-
-			<div id="contact" class="lowerBoxes box">
-				<h1 id="contactTittle">Contacto</h1>
-				<ul class="contactText">
-					<li>Cra 123 Risque 67-89</li>
-					<li>storres@exeamedia.com</li>
-					<li>3251888</li>
-					<li>3152238974</li>
-				</ul>
-			</div>
-
-			<div id="networks" class="lowerBoxes box">
-				<h1 id="networksTittle">Redes         
-					<div class="socialImg">
-						<a href="https://twitter.com/EnergiaFmOnline" id="twButton"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-tw.png" alt="">		<a href="https://www.facebook.com/EnergiaFmOnline" id="fbButton"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/icon-fb.png" alt="" width="7px"></a>
-					</div>     
-				</h1>
-			</div>
-		</div>	
-
-		<footer>
-			<div id="info">
-				<div id="license">
-					<p>Copyright 2013. Energia. Todos los derechos reservados</p>
-				</div>
-				<div id="credits">
-					<p>Desarrollado por <a href="http://exeamedia.com">Exea</a></p>
-				</div>
-			</div>
-		</footer>
+			</footer>
+		</div>
 
 	</body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,12 +1,75 @@
- $(document).ready(function() {
+// Setup energía player
+$(document).ready(function() {
+    
+    var stream = {
+        title: "Energia FM",
+        mp3: "http://stream02.exeamedia.com:8000/energiaweb02.mp3"
+    },
+    ready = false;
+
     $("#jquery_jplayer_1").jPlayer({
-        ready: function(event) {
-            $(this).jPlayer("setMedia", {
-                mp3: "http://jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-                oga: "http://jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
-            });
+        ready: function (event) {
+            ready = true;
+            $(this).jPlayer("setMedia", stream);
         },
-        swfPath: "http://jplayer.org/latest/js",
-        supplied: "mp3, oga",
+        pause: function() {
+            $(this).jPlayer("clearMedia");
+        },
+        error: function(event) {
+            if(ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
+                // Setup the media stream again and play it.
+                $(this).jPlayer("setMedia", stream).jPlayer("play");
+            }
+        },
+        swfPath: "js",
+        supplied: "mp3",
+        preload: "none",
+        wmode: "window",
+        keyEnabled: true,
+        cssSelectorAncestor: "#jp_container_1",
+        autoPlay: true
     });
 });
+
+// Facebook functions
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Twitter functions
+!function(d,s,id){
+    var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+    if(!d.getElementById(id)){
+        js=d.createElement(s);
+        js.id=id;js.src=p+"://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js,fjs);
+    }
+}(document,"script","twitter-wjs");
+
+(function($){
+    $(window).load(function(){
+        // Custom scrollbar for news
+        $("#newsContainer").mCustomScrollbar();
+
+        // Put styles for tweets tittle
+        setTimeout(function() {
+            var $head = $("#twitter-widget-0").contents().find("head");                
+            $head.append($("<link/>", { rel: "stylesheet", href: '//fonts.googleapis.com/css?family=Podkova:400,700', type: "text/css" }));
+        }, 1); 
+
+        setTimeout(function(){
+            $("#twitter-widget-0").contents().find('h1.summary a').css("font-size", "20px");
+            $("#twitter-widget-0").contents().find('h1.summary a').css("font-family", "'Podkova', Sans-Serif");
+            $("#twitter-widget-0").contents().find('h1.summary a').css("color", "#49524C");
+            $("#twitter-widget-0").contents().find('h1.summary a').css("text-transform", "uppercase");
+            $("#twitter-widget-0").contents().find('h1.summary a').css("text-decoration", "none");
+            $("#twitter-widget-0").contents().find('h1.summary a').css("padding-left", "10px");
+        }, 500);
+    });
+
+})(jQuery);
+
